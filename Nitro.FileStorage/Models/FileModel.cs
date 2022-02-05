0bf9bf6443fec0757185504218440245769d5704
info.aliahmadi <info.aliahmadi@gmail.com>
2022-02-06 01:01:51 +0330

    Add File Storage Feature

---
+++ Nitro.FileStorage/Models/FileModel.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Nitro.FileStorage.Models
+{
+    public class FileModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = null!;
+
+        public string FileName { get; set; } = null!;
+
+        public string UploadDateTime { get; set; } = null!;
+
+        public long Length { get; set; }
+
+        public string UserId { get; set; } = null!;
+
+    }
+}
