63e369c1ce93e2d5d5eba7a37074b0ad024ca033
info.aliahmadi <info.aliahmadi@gmail.com>
2022-10-03 22:31:12 +0330

    Add Test

---
+++ Nitro.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+namespace Nitro.Test
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
