c35af1fee625c71bbef192fc97487e4b3c308afc
infoaliahmadi <info.aliahmadi@gmail.com>
2022-10-12 11:22:36 +0330

    Add New Library

---
+++ Nitro.Infrastructure.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Nitro.Infrastructure.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Nitro.Web.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Nitro.Web.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
